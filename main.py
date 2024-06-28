a27a0d3b29823373ccad9bddd1c677cf8e74b75a
LegosAndStuff <masonnorberg@midco.net>
2024-06-28 11:11:38 -0500

    added frames

--- main.py
+++ main.py
@@ -6,6 +6,14 @@ import os
 
 root = customtkinter.CTk()
 root.title("Timer")
-root.geometry("400x200")
+root.geometry("425x210")
+
+root.columnconfigure((0,1,2), weight=1)
+
+timeArea = customtkinter.CTkFrame(root)
+timeArea.grid(column=0, row=0, padx=5, pady=5)
+
+workplace = customtkinter.CTkFrame(root)
+workplace.grid(column=1, row=0, columnspan=2, padx=5, pady=5)
 
 root.mainloop()
